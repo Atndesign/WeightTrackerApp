70186defbe6c0d6a0ef5526528994a9c49a6bb79
Kevin <Atnnetworkk@gmail.com>
2020-02-12 19:45:57 +0100

    Add APP root components

---
+++ src/components/Goal.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Goal extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default Goal;
