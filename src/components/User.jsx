18ffb88e181e24cf3919c67af3876437c5530c61
Kevin <Atnnetworkk@gmail.com>
2020-02-12 19:51:21 +0100

    Add state and greeting

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 class User extends Component {
-  state = {};
+  state = {
+    username: "Lily",
+    avatar:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BODFjZTkwMjItYzRhMS00OWYxLWI3YTUtNWIzOWQ4Yjg4NGZiXkEyXkFqcGdeQXVyMTQ0ODAxNzE@._V1_UX172_CR0,0,172,256_AL_.jpg"
+  };
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <header>
+        <div className="profilePic">
+          <img
+            className="profilePic__img"
+            src={this.state.avatar}
+            alt=""
+            width="30px"
+          />
+        </div>
+        <p>Hello {this.state.username}</p>
+      </header>
     );
   }
 }
