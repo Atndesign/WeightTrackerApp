ca4f3bacb3410716da19de937076c005f3d0f27a
Kevin <Atnnetworkk@gmail.com>
2020-02-13 15:50:31 +0100

    Implement style of the Bmi calc component

--- src/components/Bmi.jsx
+++ src/components/Bmi.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 
 class Bmi extends Component {
-  state = {};
+  state = {
+    weight: 69,
+    isHealthy: true
+  };
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <h2 className="section-title">BMI calculator</h2>
+        <div className="bmi">
+          <p className="bmi__state">
+            <b>{this.state.weight}</b>{" "}
+            {this.state.isHealthy && "You're doing good 🏆"}
+          </p>
+          <div className="bmi__indicator">
+            <div className="bmi-indicator__slider"></div>
+          </div>
+        </div>
       </div>
     );
   }
