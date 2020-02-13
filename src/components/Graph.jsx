c6e4ac09da197ad80a79d4f13d7abdf1dc9cd595
Kevin <Atnnetworkk@gmail.com>
2020-02-13 01:08:03 +0100

    Add ApexChart for showing chart

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,12 +1,58 @@
 import React, { Component } from "react";
+import Chart from "react-apexcharts";
 
 class Graph extends Component {
-  state = {};
+  state = {
+    options: {
+      chart: {
+        id: "basic-bar",
+        toolbar: {
+          show: false
+        }
+      },
+      fill: {
+        colors: ["#59E1FF", "#E91E63", "#9C27B0"]
+      },
+      xaxis: {
+        lines: {
+          show: false
+        },
+        labels: {
+          show: false
+        }
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: ["#fff", "#fff", "#fff", "#fff", "#fff", "#fff"]
+          }
+        }
+      },
+      grid: {
+        borderColor: "#16172e",
+        strokeDashArray: 0
+      },
+      dataLabels: {
+        enabled: false
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91, 91, 91, 91].reverse()
+      }
+    ]
+  };
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Chart
+        className="weightChart"
+        options={this.state.options}
+        series={this.state.series}
+        type="bar"
+        height="150"
+        width={document.innerWidth}
+      />
     );
   }
 }
