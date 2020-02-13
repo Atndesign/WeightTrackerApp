c7d95fe9e7c17b61de4d02bd4c925de158e230ea
Kevin <Atnnetworkk@gmail.com>
2020-02-13 01:08:17 +0100

    Add goal labels

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
 
 class Goal extends Component {
-  state = {};
+  constructor() {
+    super();
+    this.state = {
+      user: {
+        higher: 75,
+        current: 65,
+        goal: 52
+      }
+    };
+    this.progressBar = React.createRef();
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <div className="progress-labels">
+          <p className="progress-sm">{this.state.user.higher}</p>
+          <p className="progress-current">{this.state.user.current}</p>
+          <p className="progress-sm">{this.state.user.goal}</p>
+        </div>
+        <div ref={this.progressBar} className="progressBar">
+          <div
+            width={this.progressBar.width}
+            className="progressBar__fluid"
+          ></div>
+        </div>
       </div>
     );
   }
