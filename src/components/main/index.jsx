f472ed2975abbde57e4d3b03199ba3cb0c5971f5
Kevin <Atnnetworkk@gmail.com>
2020-02-18 19:25:28 +0100

    Add state switch to open the add weight form

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -4,20 +4,36 @@ import Goal from "./Goal";
 import Graph from "./Graph";
 import HistoryList from "./history/HistoryList";
 import User from "./User";
+import AddWeight from "./AddWeight";
 class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      add: false
+    };
   }
+
+  addWeight = () => {
+    this.setState({ add: true });
+  };
+
   render() {
-    return (
-      <React.Fragment>
-        <User />
-        <Graph />
-        <Goal />
-        <Bmi />
-        <HistoryList />
-      </React.Fragment>
-    );
+    if (this.state.add) {
+      return <AddWeight />;
+    } else {
+      return (
+        <React.Fragment>
+          <button className="add-weight__btn" onClick={this.addWeight}>
+            Add weight
+          </button>
+          <User />
+          <Graph />
+          <Goal />
+          <Bmi />
+          <HistoryList />
+        </React.Fragment>
+      );
+    }
   }
 }
 
