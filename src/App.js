532c517178fd986e9ae448ec60b554bbd603fe2c
Kevin <Atnnetworkk@gmail.com>
2020-02-18 19:26:16 +0100

    Add other components on the root

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import "./App.css";
-import Bmi from "./components/Bmi";
-import Goal from "./components/Goal";
-import Graph from "./components/Graph";
-import HistoryList from "./components/HistoryList";
-import User from "./components/User";
-
+import Main from "./components/main";
+import User from "./components/user";
+import LogginPage from "./components/loggin";
 function App() {
   return (
     <div className="App">
-      <User />
-      <Graph />
-      <Goal />
-      <Bmi />
-      <HistoryList />
+      <div className="container">
+        {/* <LogginPage /> */}
+        <Main />
+        {/* <User /> */}
+      </div>
     </div>
   );
 }
