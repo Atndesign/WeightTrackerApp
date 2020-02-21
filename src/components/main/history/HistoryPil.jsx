f9809c41a57a9aeab1da76f623743643ab9c1522
Kevin <Atnnetworkk@gmail.com>
2020-02-21 21:28:12 +0100

    comment code ftm

--- src/components/main/history/HistoryPil.jsx
+++ src/components/main/history/HistoryPil.jsx
@@ -2,22 +2,45 @@ import React, { Component } from "react";
 
 class HistoryPil extends Component {
   state = {
-    isMoreThanBefore: true
+    isMoreThanBefore: true,
+    weight: 0
   };
-
+  // componentDidMount() {
+  //   this.weightLost();
+  // }
+  // weightIndicator() {
+  //   if (this.state.isMoreThanBefore && "+") {
+  //     return (
+  //       <p className="historyPil-right__comparaison --green">
+  //         {this.state.weight}
+  //       </p>
+  //     );
+  //   } else {
+  //     return (
+  //       <p className="historyPil-right__comparaison --red">
+  //         {this.state.weight}
+  //       </p>
+  //     );
+  //   }
+  // }
+  // weightLost() {
+  //   let weight =
+  //     this.props.weightList[this.props.index] -
+  //     this.props.weightList[
+  //       this.props.weightList.length - (this.props.index - 1)
+  //     ];
+  //   this.setState({
+  //     weight: weight
+  //   });
+  // }
   render() {
     return (
       <div className="historyPil">
         <div className="historyPil__left">
-          <p className="historyPil-right__date">Today</p>
-          <p className="historyPil-right__comparaison">
-            {(this.state.isMoreThanBefore && "+") ||
-              (!this.state.isMoreThanBefore && "-")}
-            15kg
-          </p>
+          <p className="historyPil-right__date">{this.props.day}</p>
         </div>
         <div className="historyPil__right">
-          <p className="historyPil-right__weight">62.5</p>
+          <p className="historyPil-right__weight">{this.props.weight}</p>
         </div>
       </div>
     );
