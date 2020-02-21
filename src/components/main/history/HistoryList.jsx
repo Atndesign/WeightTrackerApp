3cfb16ef18c2f0007039b7c0294e438f522bd41a
Kevin <Atnnetworkk@gmail.com>
2020-02-21 21:27:59 +0100

    Change value and pass props

--- src/components/main/history/HistoryList.jsx
+++ src/components/main/history/HistoryList.jsx
@@ -3,20 +3,21 @@ import HistoryPil from "./HistoryPil";
 
 class HistoryList extends Component {
   state = {
+    weight: [80, 76, 78],
     data: [
       {
-        day: "today",
+        day: "12 oct 2018",
         weight: 68,
         bmi: 23
       },
       {
-        day: "yesterday",
+        day: "11 oct 2018",
         weight: 70,
         bmi: 26
       },
       {
         day: "10 oct 2018",
-        weight: 68,
+        weight: 80,
         bmi: 24
       }
     ]
@@ -25,8 +26,16 @@ class HistoryList extends Component {
     return (
       <div>
         <h2 className="section-title">History</h2>
-        {this.state.data.map(e => {
-          return <HistoryPil />;
+        {this.state.data.map((e, index) => {
+          return (
+            <HistoryPil
+              weightList={this.state.weight}
+              key={e.day}
+              weight={e.weight}
+              day={e.day}
+              index={index}
+            />
+          );
         })}
       </div>
     );
