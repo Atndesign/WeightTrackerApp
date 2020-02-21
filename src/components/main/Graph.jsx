2039648110928ead3e7cfa0be295160eb11f3dad
Kevin <Atnnetworkk@gmail.com>
2020-02-21 20:26:19 +0100

    Add props super

--- src/components/main/Graph.jsx
+++ src/components/main/Graph.jsx
@@ -2,47 +2,51 @@ import React, { Component } from "react";
 import Chart from "react-apexcharts";
 
 class Graph extends Component {
-  state = {
-    options: {
-      chart: {
-        id: "basic-bar",
-        toolbar: {
-          show: false
-        }
-      },
-      fill: {
-        colors: ["#59E1FF", "#E91E63", "#9C27B0"]
-      },
-      xaxis: {
-        lines: {
-          show: false
-        },
-        labels: {
-          show: false
-        }
-      },
-      yaxis: {
-        labels: {
-          style: {
-            colors: ["#fff", "#fff", "#fff", "#fff", "#fff", "#fff"]
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: {
+        chart: {
+          id: "basic-bar",
+          toolbar: {
+            show: false
           }
+        },
+        fill: {
+          colors: ["#59E1FF", "#E91E63", "#9C27B0"]
+        },
+        xaxis: {
+          lines: {
+            show: false
+          },
+          labels: {
+            show: false
+          }
+        },
+        yaxis: {
+          labels: {
+            style: {
+              colors: ["#fff", "#fff", "#fff", "#fff", "#fff", "#fff"]
+            }
+          }
+        },
+        grid: {
+          borderColor: "#16172e",
+          strokeDashArray: 0
+        },
+        dataLabels: {
+          enabled: false
         }
       },
-      grid: {
-        borderColor: "#16172e",
-        strokeDashArray: 0
-      },
-      dataLabels: {
-        enabled: false
-      }
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91, 91, 91, 91].reverse()
-      }
-    ]
-  };
+      series: [
+        {
+          name: "series-1",
+          data: [...this.props.weightList].reverse()
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <Chart
