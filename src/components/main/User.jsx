6e0d4d148f931570cfc575be83eb731c647a8771
Kevin <Atnnetworkk@gmail.com>
2020-02-22 21:41:15 +0100

    load dynamic username

--- src/components/main/User.jsx
+++ src/components/main/User.jsx
@@ -2,21 +2,18 @@ import React, { Component } from "react";
 
 class User extends Component {
   state = {
-    username: this.props.username,
-    avatar:
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BODFjZTkwMjItYzRhMS00OWYxLWI3YTUtNWIzOWQ4Yjg4NGZiXkEyXkFqcGdeQXVyMTQ0ODAxNzE@._V1_UX172_CR0,0,172,256_AL_.jpg"
+    username: this.props.username
   };
+  componentDidUpdate(prevProps) {
+    if (prevProps.username !== this.props.username) {
+      this.setState({
+        username: this.props.username
+      });
+    }
+  }
   render() {
     return (
       <header className="header">
-        <div className="profilePic">
-          <img
-            className="profilePic__img"
-            src={this.state.avatar}
-            alt=""
-            width="30px"
-          />
-        </div>
         <p className="header__greeting"> Hello {this.state.username} </p>
       </header>
     );
