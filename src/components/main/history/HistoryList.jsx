90abc8ac346613ecd716f9474ac501fcefd9b966
Kevin <Atnnetworkk@gmail.com>
2020-02-23 21:40:34 +0100

    Remove states

--- src/components/main/history/HistoryList.jsx
+++ src/components/main/history/HistoryList.jsx
@@ -5,16 +5,9 @@ class HistoryList extends Component {
   state = {
     weights: []
   };
-  componentDidUpdate(prevProps) {
-    if (prevProps.weightList !== this.props.weightList) {
-      this.setState({
-        weights: this.props.weightList
-      });
-    }
-  }
   render() {
     return (
-      <div>
+      <div className="history-list">
         <h2 className="section-title">History</h2>
         {this.state.weights.reverse().map((e, index) => {
           return <HistoryPil key={e.Kg} weight={e.Kg} index={index} />;
