da4fe11d839d4c2fcd57431c282b404225db3ee4
Kevin <Atnnetworkk@gmail.com>
2020-02-23 21:39:54 +0100

    Remove states

--- src/components/main/User.jsx
+++ src/components/main/User.jsx
@@ -1,20 +1,10 @@
 import React, { Component } from "react";
 
 class User extends Component {
-  state = {
-    username: this.props.username
-  };
-  componentDidUpdate(prevProps) {
-    if (prevProps.username !== this.props.username) {
-      this.setState({
-        username: this.props.username
-      });
-    }
-  }
   render() {
     return (
       <header className="header">
-        <p className="header__greeting"> Hello {this.state.username} </p>
+        <p className="header__greeting"> Hello {this.props.username} </p>
       </header>
     );
   }
